5ad4ea1269ce7d2b95952f044c800e356011eeb5
Abhishek <gaikwadabhishek978@gmail.com>
2020-08-07 18:28:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 java -cp /opt/spring-cloud-config-server ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher \
 --server.port=8888 \
+-Xms300m \
+-Xmx400m \
 --spring.config.name=application "$@"
